486494f560e5215091e10740c77e285f1c7fb875
Hidekazu Karino <hidekazu.karino.j@gmail.com>
2021-09-29 15:02:44 +0000

    add CalcCollatz and its test code

--- controllers/collatz.go
+++ controllers/collatz.go
@@ -7,3 +7,25 @@ import (
 func CollatzDisplayAction(c *gin.Context) {
 	c.HTML(200, "collatz.html", gin.H{})
 }
+
+func CalcNextStep(n int64) int64 {
+	// calc next step of Collatz procedure
+	if n%2 == 0 {
+		return n / 2
+	} else {
+		return 3*n + 1
+	}
+}
+
+func CalcCollatz(n int64) []int64 {
+	// calc Collatz procedure
+	sequence := []int64{n}
+	a_i := n
+	for {
+		a_i = CalcNextStep(a_i)
+		sequence = append(sequence, a_i)
+		if a_i == 1 {
+			return sequence
+		}
+	}
+}
---
+++ test/collatz_test.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"app/controllers"
+	"reflect"
+	"testing"
+)
+
+func TestCalcNextStep(t *testing.T) {
+	var result int64
+	result = controllers.CalcNextStep(2)
+	if result != 1 {
+		t.Errorf("CalcNextStep failed. expect:%d, actual:%d", 1, result)
+	}
+
+	result = controllers.CalcNextStep(3)
+	if result != 10 {
+		t.Errorf("CalcNextStep failed. expect:%d, actual:%d", 10, result)
+	}
+}
+
+func TestCalcCollatz(t *testing.T) {
+	var result []int64
+	var expected_result []int64
+
+	result = controllers.CalcCollatz(2)
+	expected_result = []int64{2, 1}
+	if !reflect.DeepEqual(result, expected_result) {
+		t.Errorf("CalcNextStep failed. expect:%d, actual:%d", expected_result, result)
+	}
+
+	result = controllers.CalcCollatz(6)
+	expected_result = []int64{6, 3, 10, 5, 16, 8, 4, 2, 1}
+	if !reflect.DeepEqual(result, expected_result) {
+		t.Errorf("CalcNextStep failed. expect:%d, actual:%d", expected_result, result)
+	}
+
+	result = controllers.CalcCollatz(11)
+	expected_result = []int64{11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1}
+	if !reflect.DeepEqual(result, expected_result) {
+		t.Errorf("CalcNextStep failed. expect:%d, actual:%d", expected_result, result)
+	}
+}
